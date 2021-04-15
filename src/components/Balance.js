29c6125123dcd16f3526161b434f2cf059726af3
Viktor <vhelfrih@gmail.com>
2021-04-15 22:03:57 +0200

    readme, input fields fixed

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -7,7 +7,7 @@ export const Balance = () => {
 
   const amounts = transactions.map(transaction => transaction.amount);
   const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
-
+  
   return (
     <>
       <h4>Your Balance</h4>
